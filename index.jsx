f7f5b1166e2d995767bb14e1249bc19f915a9b56
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-07-08 10:22:14 +0200

    React Router V6 part 6 deferred data without database

--- index.jsx
+++ index.jsx
@@ -11,7 +11,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Vans, { loader as vansLoader } from './pages/Vans/Vans';
 import VanDetail, { loader as vanDetailLoader } from './pages/Vans/VanDetail';
-import Dashboard from './pages/Host/Dashboard';
+import Dashboard, { loader as dashboardLoader } from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import HostVans, { loader as hostVansLoader } from './pages/Host/HostVans';
@@ -50,14 +50,15 @@ const router = createBrowserRouter(
         errorElement={<Error />}
         loader={vansLoader}
       />
-      <Route path="vans/:id" element={<VanDetail />} loader={vanDetailLoader} />
+      <Route
+        path="vans/:id"
+        element={<VanDetail />}
+        errorElement={<Error />}
+        loader={vanDetailLoader}
+      />
 
       <Route path="host" element={<HostLayout />}>
-        <Route
-          index
-          element={<Dashboard />}
-          loader={async ({ request }) => await requireAuth(request)}
-        />
+        <Route index element={<Dashboard />} loader={dashboardLoader} />
         <Route
           path="income"
           element={<Income />}
@@ -68,10 +69,16 @@ const router = createBrowserRouter(
           element={<Reviews />}
           loader={async ({ request }) => await requireAuth(request)}
         />
-        <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
+        <Route
+          path="vans"
+          element={<HostVans />}
+          errorElement={<Error />}
+          loader={hostVansLoader}
+        />
         <Route
           path="vans/:id"
           element={<HostVanDetail />}
+          errorElement={<Error />}
           loader={hostVanDetailLoader}
         >
           <Route
